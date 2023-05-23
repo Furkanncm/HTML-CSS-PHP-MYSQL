baa88880cf1378ffb7754f84ea55507cafd27aab
Furkanncm <120260143+Furkanncm@users.noreply.github.com>
2023-05-23 22:37:32 +0300

    Update root.php

--- root.php
+++ root.php
@@ -1,9 +1,9 @@
 <?php
 
-$root_host = "localhost";
-$root_user = "root";
-$root_pass = "";  // web hostingde şifre verilir. onu not et
-$root_name = "wtb";
+$root_host = "sql101.epizy.com";
+$root_user = "epiz_34271043";
+$root_pass = "BKdo0maffs";  // web hostingde şifre verilir. onu not et
+$root_name = "epiz_34271043_kayit_bilgi";
 
 $root = mysqli_connect($root_host, $root_user, $root_pass, $root_name);
     if(mysqli_connect_errno()){
